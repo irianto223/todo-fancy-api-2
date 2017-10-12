4f2cfa5aa985f3590f3a524ccc55738790be1f74
irianto <email": null>
2017-10-12 19:35:11 +0700

    ganti DB

--- app.js
+++ app.js
@@ -4,12 +4,21 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 app.use(cors())
 
-mongoose.connect('mongodb://irianto223:17rnysCNwxZJiig1@cluster0-shard-00-00-obtyj.mongodb.net:27017,cluster0-shard-00-01-obtyj.mongodb.net:27017,cluster0-shard-00-02-obtyj.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin', (err) => {
+mongoose.connect('mongodb://irianto223:satekambing@cluster0-shard-00-00-obtyj.mongodb.net:27017,cluster0-shard-00-01-obtyj.mongodb.net:27017,cluster0-shard-00-02-obtyj.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin', (err) => {
   if (err) {
     console.log(err);
+  } else {
+    console.log('Database connected');
   }
-  console.log('Database connected');
 })
+
+// mongoose.connect('mongodb://localhost/todo-test', (err) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log('Database connected');
+//   }
+// })
 
 const indexRouter = require('./routes/index')
 const userRouter = require('./routes/user')
